8523971deadd8bcb244806f7ee1f6044514e14fb
royvvv10 <124590499+royvvv10@users.noreply.github.com>
2023-02-06 14:21:33 +0000

    adding.project2.py

---
+++ project2.py
@@ -0,0 +1,24 @@
+print("Hello, world! This is Python 3!")
+
+##############
+
+#Quadratic Equations
+x = 19
+y = 3
+g = 5
+t = 3
+e = (pow(g,t))
+print(e)
+z = x // y + 2
+print(z)
+result = (e % z)
+print(result)
+
+################
+    
+#Steps
+
+# 1. Calculate the power of "g" and "t"
+# 2. Calculate "x" and "y"
+# 3. Add 2 to the answer of "x" and "y"
+# 4. Calculate "e" and "z"
